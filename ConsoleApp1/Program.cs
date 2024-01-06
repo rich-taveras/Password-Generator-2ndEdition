4afbb519a3472d5d306786377f42a9909134222c
rich-taveras <rtaveras@yahoo.com>
2024-01-06 12:00:19 -0500

    initial commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+using PasswordGenerator;
+
+var pwd = new Password();
+var password = pwd.Next();
+
+Console.WriteLine("Hello, Rich");
+Console.WriteLine("this is you password:");
+Console.WriteLine(password.ToString());
+
+
